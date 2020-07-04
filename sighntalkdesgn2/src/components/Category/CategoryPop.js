97a83309e6f21e6ca1c1a348c246b50d1dd214eb
Manas-sahoo-au6 <rmanas000@gmail.com>
2020-07-04 20:12:29 +0530

    updated

--- sighntalkdesgn2/src/components/Category/CategoryPop.js
+++ sighntalkdesgn2/src/components/Category/CategoryPop.js
@@ -10,17 +10,79 @@ function CategoryPop() {
           <div>
             <AccountCircleIcon />
           </div>
+          <hr style={{ border: "bold" }} />
           <div>
-           
+            <h2>I am an InterPreter</h2>
             <button
-              style={{ backgroundColor:"transparent",border:"none",color: "#4301a6",height:"50px", width: "100px", marginLeft: "100px",marginTop:"6rem" }}
+              style={{
+                backgroundColor: "transparent",
+                border: "none",
+                color: "#4301a6",
+                height: "50px",
+                width: "100px",
+                marginLeft: "100px",
+                marginTop: "6rem",
+              }}
             >
-             <div style={{marginLeft: "-6.5rem",height:"50px",marginTop:"-3.5rem"}}> <WorkIcon /></div>
+              <div
+                style={{
+                  marginLeft: "-6.5rem",
+                  height: "50px",
+                  marginTop: "-3.5rem",
+                }}
+              >
+                <WorkIcon />
+                <label
+                  htmlFor="svg"
+                  style={{ marginLeft: "7rem", fontWeight: "900" }}
+                >
+                  WORK
+                </label>
+              </div>
             </button>
           </div>
         </div>
         <div id="user" className="user">
-          <AccountCircleIcon />
+          <div style={{ marginTop: "2rem", marginLeft: "40%" }}>
+            <AccountCircleIcon />
+          </div>
+          <hr style={{ border: "bold" }} />
+          <div>
+            <div>
+              <h2>I want to find an InterPreter</h2>
+              <button
+                style={{
+                  backgroundColor: "transparent",
+                  border: "none",
+                  color: "white",
+                  height: "50px",
+                  width: "100px",
+                  marginLeft: "100px",
+                  marginTop: "6rem",
+                }}
+              >
+                <div
+                  style={{
+                    marginLeft: "5.8rem",
+                    height: "50px",
+                    marginTop: "0",
+                  }}
+                >
+                  <WorkIcon />
+                  <label
+                    htmlFor=""
+                    style={{
+                      marginLeft: "1.5rem",
+                      fontWeight: "900",
+                      fontSize: "24px",
+                    }}
+                  >
+                    FIND
+                  </label>
+                </div>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
